106d4aa732aa82d375d72e999ac75edd6c27427d
metwas <metwas.seup@gmail.com>
2025-09-03 17:12:50 +0100

    Added initial [HyperExpress] modules

---
+++ src/global/injector/ConfigurationProvider.ts
@@ -0,0 +1,57 @@
+/**
+     MIT License
+
+     @Copyright (c) Metwas
+
+     Permission is hereby granted, free of charge, to any person obtaining a copy
+     of this software and associated documentation files (the "Software"), to deal
+     in the Software without restriction, including without limitation the rights
+     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+     copies of the Software, and to permit persons to whom the Software is
+     furnished to do so, subject to the following conditions:
+
+     The above Copyright notice and this permission notice shall be included in all
+     copies or substantial portions of the Software.
+
+     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+     AUTHORS OR Copyright HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+     SOFTWARE.
+*/
+
+/**_-_-_-_-_-_-_-_-_-_-_-_-_- @Imports  _-_-_-_-_-_-_-_-_-_-_-_-_-*/
+
+import { GLOBAL_CONFIGURATION_PROVIDER, GLOBAL_LOG_PROVIDER } from "./inject.tokens";
+import { ConfigurationService, FileProvider } from "@geeko/configuration";
+import { Provider } from "@nestjs/common";
+import { LogService } from "@geeko/log";
+
+/**_-_-_-_-_-_-_-_-_-_-_-_-_-           _-_-_-_-_-_-_-_-_-_-_-_-_-*/
+
+/**
+ * Injects an instance of @see ConfigurationService
+ * 
+ * @public
+ * @returns {Provider<ConfigurationService>}
+ */
+export const injectConfigurationService = (): Provider<ConfigurationService> =>
+{
+       return {
+              provide: GLOBAL_CONFIGURATION_PROVIDER,
+              useFactory: async ( logger: LogService ): Promise<ConfigurationService> =>
+              {
+                     const provider: ConfigurationService = new ConfigurationService( new FileProvider( {
+                            path: "./app-config.json",
+                            fragment: false,
+                            logger: logger
+                     } ) );
+
+                     await provider.load();
+                     return provider;
+              },
+              inject: [ GLOBAL_LOG_PROVIDER ]
+       };
+};
