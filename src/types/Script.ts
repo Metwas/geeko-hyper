719c2156aa8d4a3b260845ebff1c6f852b4a5b8a
metwas <metwas.seup@gmail.com>
2025-09-04 10:41:15 +0100

    Added initial [Script] types

---
+++ src/types/Script.ts
@@ -0,0 +1,97 @@
+/**
+     MIT License
+
+     @Copyright (c) Metwas
+
+     Permission is hereby granted, free of charge, to any person obtaining a copy
+     of this software and associated documentation files (the "Software"), to deal
+     in the Software without restriction, including without limitation the rights
+     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+     copies of the Software, and to permit persons to whom the Software is
+     furnished to do so, subject to the following conditions:
+
+     The above Copyright notice and this permission notice shall be included in all
+     copies or substantial portions of the Software.
+
+     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+     AUTHORS OR Copyright HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+     SOFTWARE.
+*/
+
+/**
+ * Possible @see Script shell or bootstrap types
+ * 
+ * @public
+ */
+export enum ScriptShell
+{
+       WEB,
+       NODE
+};
+
+/**
+ * Core script interface options
+ * 
+ * @public
+ */
+export type Script = {
+       /**
+        * Unique identifier of the script
+        * 
+        * @public
+        * @type {String}
+        */
+       id: string;
+
+       /**
+        * Script shell type
+        * 
+        * @public
+        * @type {ScriptShell}        
+        */
+       shell: ScriptShell;
+
+       /**
+        * Unix timestamp of the time-to-live on this script therefore forcing any loaders to fetch from source if expired
+        * 
+        * @public
+        * @type {Number}
+        */
+       ttl?: number;
+
+       /**
+        * Path to the browsable script file, e.g: .html, .svg, .jsx, etc.
+        * 
+        * @public
+        * @type {String}
+        */
+       file: string;
+
+       /**
+        * Root directory which contains the @see path
+        * 
+        * @public
+        * @type {String}
+        */
+       root: string;
+
+       /**
+        * Flag to allow for automatic code injection from the core API @see HttpScriptService
+        * 
+        * @public
+        * @type {Boolean} 
+        */
+       inject?: boolean;
+
+       /**
+        * Injector token if @see inject has been enabled
+        * 
+        * @public
+        * @type {String}
+        */
+       injectToken?: string;
+};
