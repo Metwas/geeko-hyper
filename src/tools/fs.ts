a8c7a3637701894c4b509370bfa1518a88ce5eee
metwas <metwas.seup@gmail.com>
2025-09-15 13:32:00 +0100

    Added awaitable fs.stats

---
+++ src/tools/fs.ts
@@ -0,0 +1,53 @@
+/**
+     MIT License
+
+     @Copyright (c) Metwas
+
+     Permission is hereby granted, free of charge, to any person obtaining a copy
+     of this software and associated documentation files (the "Software"), to deal
+     in the Software without restriction, including without limitation the rights
+     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+     copies of the Software, and to permit persons to whom the Software is
+     furnished to do so, subject to the following conditions:
+
+     The above Copyright notice and this permission notice shall be included in all
+     copies or substantial portions of the Software.
+
+     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+     AUTHORS OR Copyright HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+     SOFTWARE.
+*/
+
+/**_-_-_-_-_-_-_-_-_-_-_-_-_- @Imports  _-_-_-_-_-_-_-_-_-_-_-_-_-*/
+
+import { exists } from "@geeko/configuration";
+import { Stats, promises } from "node:fs";
+
+/**_-_-_-_-_-_-_-_-_-_-_-_-_-           _-_-_-_-_-_-_-_-_-_-_-_-_-*/
+
+/**
+ * Awaitable FS @see Stats helper 
+ * 
+ * @public
+ * @returns {Promise<Stats>}
+ */
+export const getFsStat = async ( path: string ): Promise<Stats | undefined> =>
+{
+       try
+       {
+              if ( await exists( path ) === true )
+              {
+                     return await promises.stat( path );
+              }
+
+              return void 0;
+       }
+       catch ( error )
+       {
+              return void 0;
+       }
+};
