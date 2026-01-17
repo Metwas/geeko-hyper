a83501eeeef51d79cab0dba9b848368f9fda35b4
metwas <metwas.seup@gmail.com>
2026-01-17 19:14:03 +0000

    Updated license to GPL-2.0-only

--- src/modules/script/services/ScriptInjectorService.ts
+++ src/modules/script/services/ScriptInjectorService.ts
@@ -1,26 +1,19 @@
 /**
-     MIT License
-
-     @Copyright (c) Metwas
-
-     Permission is hereby granted, free of charge, to any person obtaining a copy
-     of this software and associated documentation files (the "Software"), to deal
-     in the Software without restriction, including without limitation the rights
-     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-     copies of the Software, and to permit persons to whom the Software is
-     furnished to do so, subject to the following conditions:
-
-     The above Copyright notice and this permission notice shall be included in all
-     copies or substantial portions of the Software.
-
-     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-     AUTHORS OR Copyright HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-     SOFTWARE.
-*/
+ * Copyright (c) Metwas
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 
 /**_-_-_-_-_-_-_-_-_-_-_-_-_- Imports  _-_-_-_-_-_-_-_-_-_-_-_-_-*/
 
@@ -30,54 +23,50 @@ import { IScriptSourceProvider } from "../interfaces/IScriptSourceProvider";
 
 /**
  * @see Script source injector service
- * 
+ *
  * @public
  */
-export class ScriptInjectorService
-{
+export class ScriptInjectorService {
        /**
         * @public
-        * @param {IScriptSourceProvider} sourceProvider 
-        * @param {LogService} logger 
+        * @param {IScriptSourceProvider} sourceProvider
+        * @param {LogService} logger
         */
-       public constructor( public readonly sourceProvider: IScriptSourceProvider )
-       {
+       public constructor(
+              public readonly sourceProvider: IScriptSourceProvider,
+       ) {
               this.load();
        }
 
        /**
         * Loads the source from the provided @see IScriptSourceProvider
-        * 
+        *
         * @public
         * @returns {Promise<void>}
         */
-       public async load(): Promise<void>
-       {
-              if ( this.sourceProvider )
-              {
+       public async load(): Promise<void> {
+              if (this.sourceProvider) {
                      await this.sourceProvider.load();
               }
        }
 
        /**
         * Returns the source from the configured @see IScriptSourceProvider
-        * 
+        *
         * @public
         * @returns {Buffer}
         */
-       public source(): Buffer | undefined
-       {
+       public source(): Buffer | undefined {
               return this.sourceProvider.source();
        }
 
        /**
         * Returns the @see source replacer/needle
-        * 
+        *
         * @public
         * @returns {Buffer}
         */
-       public needle(): Buffer | Array<Buffer> | undefined
-       {
+       public needle(): Buffer | Array<Buffer> | undefined {
               return this.sourceProvider.needle();
        }
 }
